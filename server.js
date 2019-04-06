c6eb17f70a9f57186b988345312bfc451165c5fa
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-05 19:50:52 -0600

    setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 9000
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log('Ready For Work')
+})
