cf83087e019db063851c0a0418fc5d3b764eb61b
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-10 20:11:01 -0500

    setup api module

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+const apiRouter = require('./api')
+
 mongoose.connect(config.database_url, { useNewUrlParser: true })
   .then(
     () => {
@@ -16,6 +18,8 @@ mongoose.connect(config.database_url, { useNewUrlParser: true })
 app.get('/', (req, res) => {
   res.json({ appName: 'Api checko', version: 1 })
 })
+
+app.use('/api', apiRouter)
 
 app.listen(config.port, () => {
   console.log('Ready For Work')
