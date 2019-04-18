711848bb022ed1846875c23319ded56ee680a2f2
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-17 20:36:27 -0500

    added validations and error handler

--- api/user/index.js
+++ api/user/index.js
@@ -6,14 +6,24 @@ const update = require('./update')
 const deleteUser = require('./delete')
 const me = require('./me')
 const { authMiddleware } = require('../../modules/auth')
-
+const { validateNewUserData, validateNewUserDontExists } = require('../../validators/user')
+const { LogError } = require('../../utils/Log')
 const userRouter = Router()
 
 userRouter.get('/', find)
-userRouter.post('/', save)
+userRouter.post('/', validateNewUserData, validateNewUserDontExists, LogError, save)
 userRouter.get('/me', authMiddleware, me)
 userRouter.get('/:id', findOne)
 userRouter.patch('/:id', update)
 userRouter.delete('/:id', deleteUser)
+
+userRouter.use((err, req, res, next) => {
+  const { message } = err
+  if (message === 'User Data is not valid') {
+    res.status(400).json({ message: err.message })
+  } else {
+    next(err)
+  }
+})
 
 module.exports = userRouter
